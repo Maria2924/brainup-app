9b21dcb68a68708906588482e3fb10d22c5be94f
Shindou Mihou <git@mihou.pw>
2025-04-13 03:51:14 +0800

    +add: fixed height for text input

--- src/components/themed/parts/ThemedTextInput.tsx
+++ src/components/themed/parts/ThemedTextInput.tsx
@@ -40,7 +40,7 @@ export default function ThemedTextInput(props: ThemedTextInputProps) {
             </Text>
             <TextInput
                 secureTextEntry={props.type === "password"}
-                className={"bg-[#D9D9D9] px-4 py-3 border border-black rounded-lg text-black font-insB text-lg"}
+                className={"bg-[#D9D9D9] px-4 h-16 border border-black rounded-lg text-black font-insB text-lg"}
                 placeholder={props.placeholder}
                 placeholderTextColor={"#767676"}
                 allowFontScaling={false}
