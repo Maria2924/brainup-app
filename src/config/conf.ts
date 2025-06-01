721e25bdbdb0142471b2e5ba067ba7c4291e543f
Maria2924 <maria2924@github.com>
2025-06-01 14:22:03 +0800

    add: fixes:

--- src/config/conf.ts
+++ src/config/conf.ts
@@ -1,5 +1,5 @@
 import {Capacitor} from "@capacitor/core";
 
 export const config = {
-    BACKEND_URL: Capacitor.getPlatform() === "web" ? "http://localhost:8000" : "https://concentration-forming-annotation-excess.trycloudflare.com"
+    BACKEND_URL: Capacitor.getPlatform() === "web" ? "http://localhost:8000" : "https://s21ca7i24n.sharedwithexpose.com"
 }
